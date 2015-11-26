9b9c286133907cff1ec10b199f414c64209cb0f3
Michael Persson <michael.ake.persson@gmail.com>
2015-11-26 13:26:18 +0100

    Initial stuff

---
+++ templext_test.go
@@ -0,0 +1,29 @@
+package templex
+
+import (
+	"testing"
+)
+
+func Test_Last(t *testing.T) {
+	a := []string{"1", "2"}
+	if r, _ := Last(1, a); r != true {
+		t.Error("Last didn't return expected result")
+	} else {
+		t.Log("Last test passes")
+	}
+
+	a2 := []int{1, 2}
+	if r, _ := Last(1, a2); r != true {
+		t.Error("Last didn't return expected result")
+	} else {
+		t.Log("Last test passes")
+	}
+
+	a3 := 1
+	if r, _ := Last(1, a3); r != false {
+		t.Error("Last didn't return expected result")
+	} else {
+		t.Log("Last test passes")
+	}
+
+}
